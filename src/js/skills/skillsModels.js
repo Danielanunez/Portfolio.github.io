45f044c425cbeff06e4baa87143c6039bba0dbe5
Danielanunez <naninunez0@gmail.com>
2025-12-15 15:40:15 -0300

    hover en cards

--- src/js/skills/skillsModels.js
+++ src/js/skills/skillsModels.js
@@ -1,7 +1,7 @@
 // skill contructors
 
 export const skillModel = (skill) =>
-    `<li class="p-4 bg-white/5 rounded-lg shadow w-80 mr-5 content-center" id="${skill.name}">
+    `<li class="p-4 bg-white/5 rounded-lg shadow w-full h-full mr-5 content-center hover:bg-sky-800" id="${skill.name}">
         <img class="rounded-md" src="${skill.url}" alt="${skill.name}">
-         <p class="text-white text-xl font-semibold uppercase">${skill.name}</p>
+        <p class="text-white text-xl font-semibold uppercase bg-red">${skill.name}</p>
     </li>`
